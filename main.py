5fc3bea229121550f3c65141c34acd7ae02efe54
oliverb212 <oliverb@naver.com>
2024-03-27 12:04:44 +0900

    학생 리스트 완성, 코멘트 작성란 제작중

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import json
 stu_list:dict
 role_list:dict
 
+width,height = 1920,1080
+
 
 #TODO: CVS 파일 임포트, UI 개발
 
@@ -39,40 +41,54 @@ class stu_directory():
         stu_list[stu] = {"role":role, "comment":comment, "rate":rate}
 
 class gui(QWidget):
+    stu_num = 0
+    student = []
+    
     def __init__(self):
         super().__init__()
         global stu_list
-        global role_list
-        
+        self.student = list(stu_list.keys())
+
         self.grid = QGridLayout(self)
 
         self.tableGroup = QGroupBox("학생 리스트",self)
         self.box_2 = QGroupBox("box",self)
         self.box_3 = QGroupBox("box",self)
-        self.box_4 = QGroupBox("box",self)
-        
-        #######학생 리스트 구성#############
-        self.tableLayout = QVBoxLayout(self)   
-        self.tableGroup.setLayout(self.tableLayout)
+        self.comment_box = QGroupBox(self)
 
-        self.table = QTableWidget(len(stu_list),3,self)
-        
-        student = tuple(stu_list.keys())        
-        for i in range(len(stu_list)):
-            self.table.setItem(i,0,QTableWidgetItem(student[i]))
-            self.table.setItem(i,1,QTableWidgetItem(stu_list[student[i]]['role']))
-            self.table.setItem(i,2,QTableWidgetItem(stu_list[student[i]]['comment']))
-            self.table.setItem(i,3,QTableWidgetItem(stu_list[student[i]]['rate']))
-        del student
-        
-        self.tableLayout.addWidget(self.table)
-        ###################################
+        self.tab_init()
 
-        #그리드에 그룹박스 위젯 추가
+        #코멘트 작성 라인###########
+        self.comment_layer = QVBoxLayout(self)
+        self.comment_line = QTextEdit(self)
+        self.comment_line.setText(stu_list[self.student[self.stu_num]]['comment'])
+
+        self.comment_box.setLayout(self.comment_layer)
+        self.comment_layer.addWidget(self.comment_line)
+        ##########################
+       
+        #그리드에 그룹박스 위젯 추가    
         self.grid.addWidget(self.tableGroup,0,0,Qt.AlignmentFlag.AlignLeft)
         self.grid.addWidget(self.box_2,0,1,Qt.AlignmentFlag.AlignLeft)
         self.grid.addWidget(self.box_3,1,0,Qt.AlignmentFlag.AlignLeft)
-        self.grid.addWidget(self.box_4,1,1,Qt.AlignmentFlag.AlignLeft)
+        self.grid.addWidget(self.comment_box,1,1,Qt.AlignmentFlag.AlignLeft)
+    
+    def tab_init(self):
+         #######학생 리스트 구성#############
+        self.tableLayout = QVBoxLayout(self)
+        self.table = QTableWidget(len(stu_list),3,self)
+        #self.table.setFixedSize(width,height)
+
+        for i in range(len(stu_list)):
+            self.table.setItem(i,0,QTableWidgetItem(self.student[i]))
+            self.table.setItem(i,1,QTableWidgetItem(stu_list[self.student[i]]['role']))
+            self.table.setItem(i,2,QTableWidgetItem(stu_list[self.student[i]]['comment']))
+            self.table.setItem(i,3,QTableWidgetItem(stu_list[self.student[i]]['rate']))
+        
+        self.tableGroup.setLayout(self.tableLayout)
+        self.tableLayout.addWidget(self.table)
+        ###################################
+       
 
 
 
@@ -85,7 +101,7 @@ class main():
         app = QApplication([])
         app.setApplicationDisplayName("main")
         widget = gui()
-        widget.resize(1920,1080)
+        widget.resize(width,height)
         widget.show()
         
         sys.exit(app.exec())
