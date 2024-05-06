76fe498cb5face0fecf4ada71cc93cf0bbe9dccf
oliverb212 <oliverb@naver.com>
2024-05-07 04:48:53 +0900

    commit

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ role_list:dict #자료구조: {역할, 역할,...}
 release_hash = ""
 
 
-width,height = 1600,900
+width,height = 1600,1000
 
 
-#TODO: CVS 파일 임포트 익스포트, UI 개발(ui 디자이너 사용), 테이블 수정 및 추가 기능,릴리즈 해시로 업데이트 확인,테이블 수정시 데이터 리스트 수정 
+#TODO: CVS 파일 임포트 익스포트, 테이블 수정 및 추가 기능(급함!!!!),릴리즈 해시로 업데이트 확인
 
 class check_load_file():
     def __init__(self) -> None:
@@ -24,14 +24,19 @@ class check_load_file():
         global release_hash
 
         self.check_file()
-        with open("data\\data.json",'r',encoding='utf-8') as f:
-            raw = json.loads(f.read())
-            stu_list = raw["student"]
-            role_list = raw["role"]
-            release_hash = raw['release_hash']
-
-
-
+        try:
+            with open("data\\data.json",'r',encoding='utf-8') as f:
+                raw = json.loads(f.read())
+                stu_list = raw["student"]
+                role_list = raw["role"]
+                release_hash = raw['release_hash']
+        except:
+            with open("data\\data.json",'w',encoding='utf-8') as f:
+                temp = {"student": {"예시": {"role": "예시", "comment": "예시", "rate": "5"}}, "role": {"예시":"예시입니다."}, "release_hash": ""}
+                json.dump(temp,f,ensure_ascii=False)
+                stu_list = raw["student"]
+                role_list = raw["role"]
+                release_hash = raw['release_hash']
 
     def check_file(self): #data 안에 학생, 역할, 코멘트, 평점 다 있음, 정규화 필요
         if path.isdir('data') == False:
@@ -40,12 +45,14 @@ class check_load_file():
         if path.isfile('data\data.json') == False:
             with open("data\data.json", 'w') as f:
                 pass
-
+class check_update():
+    def __init__(self) -> None:
+        pass
 class stu_directory():
     def make_json_preset(self):
         global stu_list,role_list,release_hash
         return {"student":stu_list, "role":role_list, "release_hash":release_hash}
-
+    
         
 class gui(QWidget):
     stu_num = 0
@@ -60,41 +67,54 @@ class gui(QWidget):
         global stu_list
         global width
         global height
+
+        #menubar = QMenuBar(self)
+        #menubar_file =QMenu("파일",self)
+        #menubar_add_role =QMenu("역할 추가",self)
+        #menubar_info =QMenu("정보",self)
+        #menubar.addMenu(menubar_file)
+        #menubar.addMenu(menubar_add_role)
+        #menubar.addMenu(menubar_info)
+        
         
         self.student = list(stu_list.keys())
 
         grid = QGridLayout(self)
+        #grid.setMenuBar(menubar)
         self.setLayout(grid)
 
-        self.tableGroup = QGroupBox("학생 리스트",self)
-        self.tableGroup.setMinimumSize(QSize(width/2,height/2.5))
-
+        self.table_group = QGroupBox("학생 리스트",self)
         self.setting_box = QGroupBox("학생 추가",self)
         self.detail_box = QGroupBox("세부사항",self)
-        self.comment_box = QGroupBox(self)
+        self.comment_box = QGroupBox("코멘트(쓰이는 즉시 자동저장)",self)
 
-        self.comment_box.setMinimumSize(QSize(width/2,height/1.5))
-        self.detail_box.setMinimumSize(width-self.comment_box.size().width(),height-self.comment_box.size().height())
+        self.table_group.setMinimumSize(width/2,800)
+        self.setting_box.setMinimumSize(width/2,200)
+        self.detail_box.setMinimumSize(width/2,200)
+        self.comment_box.setMinimumSize(width/2,800)
+        
+
         
         #그리드에 그룹박스 위젯 추가    
-        grid.addWidget(self.tableGroup,0,0,Qt.AlignmentFlag.AlignLeft)
-        grid.addWidget(self.detail_box,0,1,Qt.AlignmentFlag.AlignLeft)
-        grid.addWidget(self.setting_box,1,0,Qt.AlignmentFlag.AlignLeft)
-        grid.addWidget(self.comment_box,1,1,Qt.AlignmentFlag.AlignLeft)
+        grid.addWidget(self.table_group,0,0,Qt.AlignmentFlag.AlignTop)
+        grid.addWidget(self.detail_box,1,1,Qt.AlignmentFlag.AlignTop)
+        grid.addWidget(self.setting_box,1,0,Qt.AlignmentFlag.AlignTop)
+        grid.addWidget(self.comment_box,0,1,Qt.AlignmentFlag.AlignTop)
         
         self.table_init()
         self.comment_init()
         self.detail_init()
         self.setting_init()
-        self.update_text()
+        self.update_all()
     
     #--------------slots----------------
+
     @Slot()
     def stu_select_event(self):
         action:QAction = self.sender()
         self.stu_num = action.data()
         #이닛을 부른다 해도 택스트 업데이트 X
-        self.update_text()
+        self.update_all()
 
     @Slot()
     def randomize_stu(self):
@@ -108,7 +128,7 @@ class gui(QWidget):
         random.shuffle(role)
         for i in range(len(self.student)): #섞은 리스트를 다시 원래 리스트에 붙여넣는 작업
             stu_list[self.student[i]]['role'] = role[i]
-        self.update_text()
+        self.update_all()
 
     
     @Slot()
@@ -131,28 +151,54 @@ class gui(QWidget):
     
     @Slot()
     def delete_cell(self):
-        self.table.removeRow(self.cell_row)
+        item_name = self.table.item(self.cell_row,1).text()
+        del stu_list[item_name]
     
+        self.table.removeRow(self.cell_row)
+        self.update_all()
+    
+
     @Slot()
-    def test(self):
-        sender:QTableWidget = self.sender()
-        print(sender.currentItem().text())
-    #------------------------------------
+    def add_item(self):
+        input_stu = self.preset_name.text()
+        input_role = self.preset_role.currentText()
+        input_rate = self.preset_rate.text()
         
-    def update_text(self):
-        for i in range(len(stu_list)):
+        preset = {'role':'','comment':'','rate':''}
+        preset['role'] = input_role
+        preset['comment'] = ""
+        preset['rate'] = input_rate
+
+        stu_list[input_stu] = preset
+
+        self.update_all()
+
+    @Slot()
+    def update_table(self):
+        for i in range(len(self.student)):
             self.table.setItem(i,0,QTableWidgetItem(stu_list[self.student[i]]['role']))
             self.table.setItem(i,1,QTableWidgetItem(self.student[i]))
             self.table.setItem(i,2,QTableWidgetItem(stu_list[self.student[i]]['comment']))
-            self.table.setItem(i,3,QTableWidgetItem(stu_list[self.student[i]]['rate']))
+        self.table.horizontalHeader().setSectionResizeMode(2,QHeaderView.ResizeMode.Stretch)
+    
+    @Slot()
+    def update_all(self):
+        self.student = list(stu_list.keys())
+        self.update_table()
+        self.update_detail()
+    
+    #------------------------------------
+
+    def update_detail(self):
         self.comment_line.setText(stu_list[self.student[self.stu_num]]['comment'])
         self.detail_name.setText(self.student[self.stu_num])
         self.detail_role.setText(stu_list[self.student[self.stu_num]]['role'])
         self.detail_role_explain.setText(role_list[stu_list[self.student[self.stu_num]]['role']])
         self.detail_rate.setText(str(stu_list[self.student[self.stu_num]]['rate']))
+        
         self.length_text.setText(str(self.comment_length))
         self.byte_text.setText(str(self.comment_byte))
-    
+
     def comment_text_byte_len(self):
         sender = self.sender()
         self.comment_byte = len(sender.toPlainText().encode('utf-8'))
@@ -162,27 +208,57 @@ class gui(QWidget):
         
     #---------------------inits-----------------------
     def setting_init(self):
-        setting_grid = QGridLayout
+        global role_list
 
-        setting_layout = QHBoxLayout(self)
+        setting_grid = QGridLayout(self)
+
+        setting_button_layout = QHBoxLayout(self)
         add_stu_button = QPushButton('추가',self)
+        add_stu_button.pressed.connect(self.add_item)
+
         del_selected_row_button = QPushButton('선택된 열 삭제',self)
         del_selected_row_button.pressed.connect(self.delete_cell)
+        
         randomize_role_button = QPushButton("역할 랜덤 부여",self)
         randomize_role_button.pressed.connect(self.randomize_stu)
 
-        setting_layout.addWidget(add_stu_button)
-        setting_layout.addWidget(del_selected_row_button)
-        setting_layout.addWidget(randomize_role_button)
-        self.setting_box.setLayout(setting_layout)
+        setting_preset_layout = QHBoxLayout(self)
+        
+        self.preset_role = QComboBox(self)
+        self.preset_role.setBaseSize(500,100)
+        role = list(role_list.keys())
+        
+        for i in range(len(role)):
+            self.preset_role.addItem(role[i])
+        
+        self.preset_name = QLineEdit(self)
+        self.preset_name.setPlaceholderText("여기에 이름 입력")
+        
+        self.preset_rate = QLineEdit(self)
+        self.preset_rate.setMaximumWidth(100)
+        self.preset_rate.setPlaceholderText("여기에 점수 입력")
+
+        setting_preset_layout.addWidget(self.preset_role)
+        setting_preset_layout.addWidget(self.preset_name)
+        setting_preset_layout.addWidget(self.preset_rate)
+        
+        setting_button_layout.addWidget(add_stu_button)
+        setting_button_layout.addWidget(del_selected_row_button)
+        setting_button_layout.addWidget(randomize_role_button)
+        
+        setting_grid.addLayout(setting_preset_layout,0,0)
+        setting_grid.addLayout(setting_button_layout,1,0)
+        
+        self.setting_box.setLayout(setting_grid)
+        
 
     def table_init(self):
         table_layout = QVBoxLayout(self)
         self.table = QTableWidget(len(stu_list),3,self)
         self.table.cellClicked.connect(self.cell_select)
-        self.table.itemSelectionChanged.connect(self.test)
+        self.table.itemSelectionChanged.connect(self.update_table)
 
-        self.tableGroup.setLayout(table_layout)
+        self.table_group.setLayout(table_layout)
         table_layout.addWidget(self.table)
 
     def comment_init(self):
@@ -200,15 +276,15 @@ class gui(QWidget):
         self.detail_name = QPushButton(self)
         self.detail_name.setMaximumWidth(90)
         
-        menu = QMenu(self)
+        self.detail_menu = QMenu(self)
 
         for i in range(len(self.student)):
             act = QAction(self.student[i],self)
             act.setData(i)
             act.triggered.connect(self.stu_select_event)
-            menu.addAction(act)
+            self.detail_menu.addAction(act)
 
-        self.detail_name.setMenu(menu)
+        self.detail_name.setMenu(self.detail_menu)
         
         #nameline--------
 
@@ -250,7 +326,6 @@ class main():
         widget.resize(width,height)        
         widget.show()
         app.exec()
-        temp:dict
         #save_part
         with open('data\\data.json','w+',encoding='utf8') as f:
             json.dump(stu_directory().make_json_preset(),f,ensure_ascii=False)
