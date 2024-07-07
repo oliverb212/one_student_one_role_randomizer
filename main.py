dd7954994d45197147c97c372a77f959494e5015
oliverb212 <oliverb@naver.com>
2024-07-08 04:37:01 +0900

    almost final commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -6,22 +7,21 @@ from os import path,mkdir
 import sys
 import json
 
+from PySide6.QtWidgets import QWidget
+
 
 stu_list:dict #자료 구조: 이름:{역할, 코멘트},{...}
 role_list:dict #자료구조: {역할, 역할,...}
-release_hash = ""
 
 
 width,height = 1600,1000
 
 
-#TODO: CVS 파일 임포트 익스포트, 테이블 수정 및 추가 기능이랑 역할 추가 및 삭제(급함!!!!),릴리즈 해시로 업데이트 확인
-
+#TODO: CVS 파일 임포트 익스포트(이부분은 파일 path 가저오고 프로그램 재로드하는 방식으로)
 class check_load_file():
     def __init__(self) -> None:
         global stu_list
         global role_list
-        global release_hash
 
         self.check_file()
         try:
@@ -29,16 +29,17 @@ class check_load_file():
                 raw = json.loads(f.read())
                 stu_list = raw["student"]
                 role_list = raw["role"]
-                release_hash = raw['release_hash']
+                if (stu_list == {}) or (role_list == {}):
+                    raise ValueError()
         except:
             with open("data\\data.json",'w+',encoding='utf-8') as f:
-                temp = {"student": {"예시": {"role": "예시", "comment": "예시", "rate": "5"}}, "role": {"예시":"예시입니다."}, "release_hash": ""}
+                temp = {"student": {"예시": {"role": "예시", "comment": "예시", "rate": "5"}}, "role": {"예시":"예시입니다."}}
                 json.dump(temp,f,ensure_ascii=False)
-                f.flush()
+                
+            with open("data\\data.json",'r',encoding='utf-8') as f:
                 raw = json.loads(f.read())
                 stu_list = raw["student"]
                 role_list = raw["role"]
-                release_hash = raw['release_hash']
 
     def check_file(self): #data 안에 학생, 역할, 코멘트, 평점 다 있음, 정규화 필요
         if path.isdir('data') == False:
@@ -48,16 +49,150 @@ class check_load_file():
         if path.isfile('data\data.json') == False:
             with open("data\data.json", 'w') as f:
                 pass
-class check_update():
+
+class load_csv():
+    def __init__(self,path) -> None:
+        import csv
+        try:
+            data = {"student":{},"role":{}}
+
+            with open(path,"r",encoding="cp949") as f:
+                reader = csv.reader(f)
+
+                stu_name = []
+                stu_role = []
+                stu_rate = []
+                stu_comment =[]
+                role_comment = []
+                data_temp = {}
+
+                for line in reader:
+                    stu_name.append(line[0])
+                    stu_role.append(line[1])
+                    stu_comment.append(line[2])
+                    stu_rate.append(line[3])
+                    role_comment.append(line[4])
+
+                for i in range(len(stu_name)):
+                    data["student"][stu_name[i]] = {"role":stu_role[i],"comment":stu_comment[i],"rate":stu_rate[i]}              
+                    data_temp[stu_role[i]] = role_comment[i]
+
+                data["role"] = data_temp
+            
+            with open(".\\data\\data.json",'w',encoding='utf-8') as f:
+                json.dump(data,f,ensure_ascii=False)
+            
+            check_load_file()
+        
+        except Exception as e:
+            errmsg = QErrorMessage()
+            errmsg.showMessage("에러, 파일이 손상되었거나, 코드의 버그입니다. 예외 로그:{}".format(e))
+            errmsg.exec()
+
+class export_csv():
     def __init__(self) -> None:
-        pass
+        import csv
+        raw = None
+        with open(".\\data\\data.json",'r',encoding='utf-8') as f:
+            raw = json.loads(f.read())
+
+        with open(path,'w',encoding='utf-8') as f:
+            pass
+
+
+
+
+        
+
 class stu_directory():
     def make_json_preset(self):
-        global stu_list,role_list,release_hash
-        return {"student":stu_list, "role":role_list, "release_hash":release_hash}
+        global stu_list,role_list
+        return {"student":stu_list, "role":role_list}
     
+
+class role_setting_window(QWidget):
+    def __init__(self,main_gui_instance):
+        global role_list
+        super().__init__()
+        self.main_gui = main_gui_instance
         
-class gui(QWidget):
+        self.role = list(role_list.keys())
+        self.role_desc = list(role_list.values())
+
+        main_layout = QVBoxLayout(self)
+        self.setLayout(main_layout)
+        
+        self.table = QTableWidget(self)
+        self.table.setRowCount(len(self.role))
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderLabels(['역할', '설명'])
+        self.table.setEditTriggers(QTableWidget().EditTrigger(0))
+        
+        self.update()
+        
+        # 디자인
+        pannel_grid = QGridLayout()
+        self.pannel_role = QLineEdit()
+        self.pannel_role_desc = QLineEdit()
+        pannel_role_add = QPushButton()
+        pannel_role_del = QPushButton()
+
+        pannel_role_add.setText("추가")
+        pannel_role_del.setText("선택한 역할 삭제")
+        self.pannel_role.setPlaceholderText("여기에 역할 입력 (중복되는 역할 이름 불가)")
+        self.pannel_role_desc.setPlaceholderText("여기에 역할 설명 입력")
+
+        pannel_grid.addWidget(self.pannel_role,0,0)
+        pannel_grid.addWidget(pannel_role_add,0,1)
+        pannel_grid.addWidget(self.pannel_role_desc,1,0)
+        pannel_grid.addWidget(pannel_role_del,1,1)
+
+        # 디자인 끝
+
+        pannel_role_add.clicked.connect(self.add_button_func)
+        pannel_role_del.clicked.connect(self.del_button_func)
+        
+        main_layout.addWidget(self.table)
+        main_layout.addLayout(pannel_grid)
+
+    def update(self):
+        self.table.setRowCount(len(self.role)) #우라질 그놈의 함수, 이거 추가하니깐 또 멀정하게 작동함
+        for i in range(len(self.role)):
+            self.table.setItem(i,0,QTableWidgetItem(self.role[i]))
+            self.table.setItem(i,1,QTableWidgetItem(self.role_desc[i]))
+        self.table.horizontalHeader().setSectionResizeMode(1,QHeaderView.ResizeMode.Stretch)
+        main_update = QAction(self)
+        main_update.triggered.connect(main_gui().update_all)
+
+
+
+    @Slot()
+    def add_button_func(self):
+        text_role = self.pannel_role.text().strip()
+        text_desc = self.pannel_role_desc.text().strip()
+        if text_role not in role_list:
+            self.role.append(text_role)
+            self.role_desc.append(text_desc)
+            role_list[text_role] = text_desc
+            self.main_gui.update_preset_role() #이부분은 GPT 생성, 객체 인자 불러와서 가능했다는데 이부분은 나중에 물어보기
+            self.update()
+        else:
+            msg = QErrorMessage()
+            msg.showMessage("역할 이름 중복입니다, 다시 지어주세요.")
+            msg.exec()
+
+    @Slot()
+    def del_button_func(self):
+        selected_items = self.table.selectedItems()
+        if selected_items:
+            selected_row = self.table.row(selected_items[0])
+            role_to_delete = self.role[selected_row]
+            del role_list[role_to_delete]
+            del self.role[selected_row]
+            del self.role_desc[selected_row]
+            self.update()
+        
+class main_gui(QWidget):
     stu_num = 0
     student = []
     comment_byte=0
@@ -71,19 +206,36 @@ class gui(QWidget):
         global width
         global height
 
-        #menubar = QMenuBar(self)
-        #menubar_file =QMenu("파일",self)
-        #menubar_add_role =QMenu("역할 추가",self)
-        #menubar_info =QMenu("정보",self)
-        #menubar.addMenu(menubar_file)
-        #menubar.addMenu(menubar_add_role)
-        #menubar.addMenu(menubar_info)
-        
-        
-        self.student = list(stu_list.keys())
+        self.maker_window = None
+        self.role_window = None
 
+        menubar = QMenuBar(self)
+        menubar_file =QMenu("파일",self)
+        menubar_info =QMenu("정보",self)
+        
+        file_load = QAction("CSV로 로드하기",self)
+        file_export =QAction("CSV로 내보내기",self)
+        info_maker = QAction("깃허브로 이동",self)
+        
+        menubar_add_role =QAction("역할 추가",self)
+        menubar_file.addAction(file_load)
+        menubar_file.addAction(file_export)
+        menubar_info.addAction(info_maker)
+
+        info_maker.triggered.connect(self.call_maker_window)
+        menubar_add_role.triggered.connect(self.call_role_window)
+        file_load.triggered.connect(self.call_load_window)
+        
+        menubar.addMenu(menubar_file)
+        menubar.addAction(menubar_add_role)
+        menubar.addMenu(menubar_info)
+
+
+
+        self.student = list(stu_list.keys())
+    
         grid = QGridLayout(self)
-        #grid.setMenuBar(menubar)
+        grid.setMenuBar(menubar)
         self.setLayout(grid)
 
         self.table_group = QGroupBox("학생 리스트",self)
@@ -109,9 +261,35 @@ class gui(QWidget):
         self.detail_init()
         self.setting_init()
         self.update_all()
+        
     
     #--------------slots----------------
+    @Slot()
+    def call_load_window(self):
+        dbox = QFileDialog(self)
+        dbox.setNameFilter("*.CSV")
+        fpath = dbox.getOpenFileName(self,filter="*.CSV")
+        if fpath[0] != "":
+            load_csv(fpath[0])
+            self.update_all()
+    @Slot()
+    def call_role_window(self):
+        if self.role_window == None:
+            self.role_window = role_setting_window(self)
+        self.role_window.resize(640,640)
+        self.role_window.show()
 
+    def update_preset_role(self):
+        self.preset_role.clear()
+        for role in role_list.keys():
+            self.preset_role.addItem(role)
+        
+
+    @Slot()
+    def call_maker_window(self):
+        url = QUrl("https://github.com/oliverb212/one_student_one_role_randomizer")
+        QDesktopServices().openUrl(url)
+        
     @Slot()
     def stu_select_event(self):
         action:QAction = self.sender()
@@ -132,7 +310,7 @@ class gui(QWidget):
         for i in range(len(self.student)): #섞은 리스트를 다시 원래 리스트에 붙여넣는 작업
             stu_list[self.student[i]]['role'] = role[i]
         self.update_all()
-
+        
     
     @Slot()
     def comment_text_changed(self):
@@ -162,6 +340,8 @@ class gui(QWidget):
         else: pass
     
         self.table.removeRow(self.cell_row)
+        if self.table.rowCount() >= self.cell_row:
+            self.table.setRangeSelected(QTableWidgetSelectionRange(self.cell_row,0,self.cell_row,2),True)
         self.update_all()
     
 
@@ -170,32 +350,40 @@ class gui(QWidget):
         input_stu = self.preset_name.text()
         input_role = self.preset_role.currentText()
         input_rate = self.preset_rate.text()
+
+        if input_stu not in self.student:
         
-        preset = {'role':'','comment':'','rate':''}
-        preset['role'] = input_role
-        preset['comment'] = ""
-        preset['rate'] = input_rate
+            preset = {'role':'','comment':'','rate':''}
+            preset['role'] = input_role
+            preset['comment'] = ""
+            preset['rate'] = input_rate
 
-        stu_list[input_stu] = preset
+            stu_list[input_stu] = preset
 
-        self.preset_name.setText("")
-        self.preset_rate.setText("")
+            self.preset_name.setText("")
+            self.preset_rate.setText("")
 
-        self.update_all()
+            self.update_all()
+        else:
+            msgbox = QErrorMessage()
+            msgbox.showMessage("이름이 중복됩니다, 다시 지어주세요.")
+            msgbox.exec()
 
     @Slot()
     def update_table(self):
         stu_num = len(self.student)
+        self.table.setRowCount(stu_num) #이 부분 때문에 테이블 업데이트가 안된거였음;;; 고마워요 GPT4, 고마워요 스택 오버플로우
         for i in range(stu_num):
             self.table.setItem(i,0,QTableWidgetItem(stu_list[self.student[i]]['role']))
             self.table.setItem(i,1,QTableWidgetItem(self.student[i]))
             self.table.setItem(i,2,QTableWidgetItem(stu_list[self.student[i]]['comment']))
-        self.table.view
         self.table.horizontalHeader().setSectionResizeMode(2,QHeaderView.ResizeMode.Stretch)
-    
+        
+        
     @Slot()
     def update_all(self):
         self.student = list(stu_list.keys())
+
         self.update_table()
         self.update_detail()
     
@@ -205,7 +393,12 @@ class gui(QWidget):
         self.comment_line.setText(stu_list[self.student[self.stu_num]]['comment'])
         self.detail_name.setText(self.student[self.stu_num])
         self.detail_role.setText(stu_list[self.student[self.stu_num]]['role'])
-        self.detail_role_explain.setText(role_list[stu_list[self.student[self.stu_num]]['role']])
+        try:
+            self.detail_role_explain.setText(role_list[stu_list[self.student[self.stu_num]]['role']])
+        except:
+            role_list[stu_list[self.student[self.stu_num]]['role']] = "NULL"
+            self.detail_role_explain.setText(role_list[stu_list[self.student[self.stu_num]]['role']])
+        
         self.detail_rate.setText(str(stu_list[self.student[self.stu_num]]['rate']))
         
         self.length_text.setText(str(self.comment_length))
@@ -267,9 +460,10 @@ class gui(QWidget):
     def table_init(self):
         table_layout = QVBoxLayout(self)
         self.table = QTableWidget(len(stu_list),3,self)
+        self.table.setEditTriggers(QTableWidget().EditTrigger(0))
+        self.table.setHorizontalHeaderLabels(["역할","이름","코멘트"])
 
         self.table.cellClicked.connect(self.cell_select)
-        self.table.itemSelectionChanged.connect(self.update_table)
 
         self.table_group.setLayout(table_layout)
         table_layout.addWidget(self.table)
@@ -286,18 +480,9 @@ class gui(QWidget):
         detail_layout = QFormLayout(self)
         
         #name line
-        self.detail_name = QPushButton(self)
+        self.detail_name = QLabel(self)
         self.detail_name.setMaximumWidth(90)
         
-        self.detail_menu = QMenu(self)
-
-        for i in range(len(self.student)):
-            act = QAction(self.student[i],self)
-            act.setData(i)
-            act.triggered.connect(self.stu_select_event)
-            self.detail_menu.addAction(act)
-
-        self.detail_name.setMenu(self.detail_menu)
         
         #nameline--------
 
@@ -334,7 +519,7 @@ class main():
 
         app = QApplication([])
         app.setApplicationDisplayName("main")
-        self.widget = gui()
+        self.widget = main_gui()
         widget = self.widget
         widget.resize(width,height)        
         widget.show()
